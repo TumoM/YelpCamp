fe94afee11d3483f48b475f93753fb8da0fd9de7
Tumo M <tumomasire@gmail.com>
2020-01-05 18:38:23 +0200

    Using mongoose for Campgrounds

--- v2/app.js
+++ v2/app.js
@@ -1,24 +1,47 @@
-var express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var options = {'useCreateIndex': true,
+'useFindAndModify': false,
+'useNewUrlParser': true,
+'useUnifiedTopology': true
+}
+mongoose.connect("mongodb://localhost:27017/yelp_camp", options);
 
 var port = 3000;
 
-var campgrounds = [
-    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
-    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
-    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"},
-    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
-    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
-    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"},
-    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
-    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
-    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"}
-]
+// var campgrounds = [
+//     {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+//     {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+//     {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"},
+//     {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+//     {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+//     {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"},
+//     {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+//     {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+//     {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"}
+// ]
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'))
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+// Campground.create(campgrounds[1], function(err, campgrounds){
+//     if (!err) {
+//         console.log("Newly created campground");
+//         console.log(campgrounds);
+//     } else {
+//         console.log(err);
+        
+//     }
+// })
 
 app.get("/", function(req, res){
     // res.send("Landing page!!")
@@ -26,17 +49,28 @@ app.get("/", function(req, res){
 })
 
 app.get("/campgrounds", function(req, res){
-    
-res.render("campgrounds", {campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if (!err) {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        } else {
+            console.log(err);
+        }
+    })
 })
 
 app.post("/campgrounds", function(req, res){
+    
     var camp = {
         name: req.body.name,
         image: req.body.image
     }
-    campgrounds.push(camp);
-    res.redirect("/campgrounds");
+    Campground.create(camp, function(err, camps){
+        if (!err) {
+            res.redirect("/campgrounds"); 
+        } else {
+            console.log(err);
+        }
+    })
 })
 
 app.get("/campgrounds/new", function(req, res){ 
