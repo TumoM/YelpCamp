3a63e8f412c5aea464b5fc8ddd33b91be5c75662
Tumo M <tumomasire@gmail.com>
2020-01-05 16:06:00 +0200

    Added flow for adding new campgrounds

--- v1/app.js
+++ v1/app.js
@@ -1,9 +1,17 @@
 var express = require("express");
+const bodyParser = require("body-parser");
 var app = express();
 
 var port = 3000;
 
-app.set("view engine", "ejs")
+var campgrounds = [
+    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"}
+]
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     // res.send("Landing page!!")
@@ -11,12 +19,21 @@ app.get("/", function(req, res){
 })
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277bd4924cc35b_340.jpg"},
-        {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277bd4924cc35b_340.jpg"},
-        {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c72277bd4924cc35b_340.jpg"}
-    ]
+    
 res.render("campgrounds", {campgrounds});
+})
+
+app.post("/campgrounds", function(req, res){
+    var camp = {
+        name: req.body.name,
+        image: req.body.image
+    }
+    campgrounds.push(camp);
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", function(req, res){ 
+    res.render("new");
 })
 
 app.listen(port,function(){
