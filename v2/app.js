441dd99871fb90ffc28872c0b556e758844efd60
Tumo M <tumomasire@gmail.com>
2020-01-05 18:16:24 +0200

    Starting v2 - Data persistance

---
+++ v2/app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+const bodyParser = require("body-parser");
+var app = express();
+
+var port = 3000;
+
+var campgrounds = [
+    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"},
+    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"},
+    {name: "Willow Creek", image: "https://pixabay.com/get/57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Granite Fall Hills", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.jpg"},
+    {name: "Grizley Bears Gorge", image: "https://pixabay.com/get/52e3d3404a55af14f6da8c7dda793f7f1636dfe2564c704c72277adc9744c25e_340.png"}
+]
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+    // res.send("Landing page!!")
+    res.render("landing")
+})
+
+app.get("/campgrounds", function(req, res){
+    
+res.render("campgrounds", {campgrounds});
+})
+
+app.post("/campgrounds", function(req, res){
+    var camp = {
+        name: req.body.name,
+        image: req.body.image
+    }
+    campgrounds.push(camp);
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", function(req, res){ 
+    res.render("new");
+})
+
+app.listen(port,function(){
+    console.log(`YelpCamp Server running on ${port}`);
+    
+})
