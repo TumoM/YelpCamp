e4f8cce621795c10d68d2742881a44084c0f9a2e
Tumo M <tumomasire@gmail.com>
2020-01-08 17:15:18 +0200

    v8 - Comment with User association

---
+++ v8/routes/campgrounds.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var router = express.Router();
+var Campground  = require("../models/campground"),
+Comment     = require("../models/comment");
+
+
+
+
+// INDEX Campground 
+router.get("/", function (req, res) {
+    var user = req.user
+    Campground.find({}, function (err, allCampgrounds) {
+        if (!err) {
+            res.render("campgrounds/index", {
+                campgrounds: allCampgrounds
+            });
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+// CREATE Campground
+router.post("/", function (req, res) {
+
+    var camp = {
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    }
+    Campground.create(camp, function (err, camps) {
+        if (!err) {
+            res.redirect("/campgrounds");
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+// NEW Campground
+router.get("/new", function (req, res) {
+    res.render("campgrounds/new");
+})
+
+// SHOW Campground
+router.get("/:id", function (req, res) {
+    let id = req.params.id;
+    Campground.findById(id).populate("comments").exec(function (err, camp) {
+        if (!err) {
+            console.log(`Camp ${camp.name} found`);
+
+            res.render("campgrounds/show", {
+                camp
+            });
+        } else {
+            console.log(err);
+
+        }
+    });
+
+})
+
+module.exports = router;
