e828b71d314badfd58c4c7d5fb848df6c1d79684
Tumo M <tumomasire@gmail.com>
2020-01-08 23:18:41 +0200

    Secured Comments routes

--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -44,7 +44,58 @@ router.post("/",isLoggedIn, function (req, res) {
     })
 });
 
+//EDIT Comment
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+    Comment.findById(req.params.comment_id,function(err, comment){
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit",{camp_id:req.params.id,comment});
+        }
+    })
+})
+
+//UPDATE Comment
+router.put("/:comment_id", checkCommentOwnership, function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body.comments,function(err,comment){
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    })
+})
+
+//DELETE Comment
+router.delete("/:comment_id", checkCommentOwnership,function(req,res){
+    Comment.findByIdAndDelete(req.params.comment_id,function(err, deletedComment){
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    })
+})
 // Middleware
+function checkCommentOwnership(req,res,next){
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err,comment){
+            if (err) {
+                res.redirect("back");
+            } else {
+                if (comment.author.id.equals(req.user._id )) {
+                    next();   
+                }else{
+                    res.send("Not authorized to be here")
+                }
+            }
+        })
+    }else{
+        console.log("not logged in");
+        res.redirect("back");
+    }
+}
+
 function isLoggedIn(req, res, next){
     if (req.isAuthenticated()) {
         return next();
