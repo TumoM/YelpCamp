ae6d86bc60a262ea9ef372ed11d15f9d1eae0d43
Tumo M <tumomasire@gmail.com>
2020-01-09 02:13:32 +0200

    Clearing version history for deploy.

--- v6/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-var commentSchema = new mongoose.Schema({
-    author: String,
-    text: String
-})
-
-var Comment = mongoose.model("Comment", commentSchema);
-module.exports = Comment;
